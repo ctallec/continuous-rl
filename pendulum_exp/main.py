74a072bbf24c34a3971116518eea99f1f77ad736
ctallec <corentinxtallec@gmail.com>
2018-10-13 16:53:59 +0200

    Commit

--- pendulum_exp/main.py
+++ pendulum_exp/main.py
@@ -1,7 +1,6 @@
 """ Trying to solve pendulum using a robust advantage learning. """
 # pylint: disable=too-many-arguments, too-many-locals
 from functools import partial
-from itertools import chain
 from time import sleep
 import argparse
 from typing import Tuple, Union, Callable
@@ -9,13 +8,12 @@ import gym
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
-import torch.nn.functional as f
 
 from envs.vecenv import SubprocVecEnv
 from envs.pusher import PusherEnv
 from envs.utils import make_env
 from models import MLP, perturbed_output
-from stats import FloatingAvg
+from stats import FloatingAvg, penalize_mean
 from noise import ParameterNoise, ActionNoise
 
 def to_tensor(arr: np.ndarray, device: torch.device) -> torch.Tensor:
@@ -37,8 +35,8 @@ def train(
         vfunction: MLP,
         afunction: MLP,
         anoise: Union[ParameterNoise, ActionNoise],
-        optimizer: torch.optim.Optimizer,
-        scheduler: torch.optim.lr_scheduler.LambdaLR,
+        optimizers: Tuple[torch.optim.Optimizer, ...],
+        schedulers: Tuple[torch.optim.lr_scheduler.LambdaLR, ...],
         vec_env: SubprocVecEnv,
         start_obs: np.ndarray,
         dt: float,
@@ -52,6 +50,7 @@ def train(
     obs = to_tensor(start_obs, device)
 
     cum_loss = 0
+    cum_mean_loss = 0
     for _ in range(nsteps):
         # interact
         with torch.no_grad():
@@ -63,7 +62,6 @@ def train(
 
             next_obs, reward, _, _ = vec_env.step(action)
             next_obs = to_tensor(next_obs, device)
-            true_reward = reward * dt
 
 
         # learn
@@ -73,39 +71,57 @@ def train(
         max_adv = torch.max(adv, dim=1)[0]
         adv_a = adv.gather(1, indices.view(-1, 1)).squeeze()
 
-        expected_v = to_tensor(true_reward, device) + \
-            gamma ** dt * vfunction(next_obs).detach().squeeze()
-        value_floating_mean.step(expected_v)
-        dv = (expected_v - value_floating_mean.mean - v) / dt
+        discounted_next_v = gamma ** dt * vfunction(next_obs).squeeze()
+        expected_v = to_tensor((reward - value_floating_mean.mean), device) * dt + \
+            discounted_next_v.detach()
+        dv = (expected_v - v) / dt
+        value_floating_mean.step(reward)
         a_update = dv - adv_a + max_adv
 
-        loss = (a_update ** 2).mean() + (max_adv ** 2).mean()
+        adv_update_loss = (a_update ** 2).mean()
+        adv_norm_loss = (max_adv ** 2).mean()
+        mean_loss = penalize_mean(v)
+        loss = adv_update_loss + adv_norm_loss
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        scheduler.step()
+        optimizers[0].zero_grad()
+        optimizers[1].zero_grad()
+        (mean_loss / dt + loss).backward()
+        optimizers[0].step()
+        schedulers[0].step()
+        optimizers[1].step()
+        schedulers[1].step()
 
         cum_loss += loss.item()
+        cum_mean_loss += mean_loss.item()
 
         obs = next_obs
     print(f'At epoch {epoch}, '
+          f'avg_mean_loss: {cum_mean_loss / nsteps} '
           f'avg_loss: {cum_loss / nsteps}')
     return to_numpy(next_obs)
 
+def gradient_norm(model: MLP):
+    """ return norm of the gradient. """
+    grad = 0
+    with torch.no_grad():
+        for p in model.parameters():
+            grad += (p.grad ** 2).sum().item()
+    return np.sqrt(grad)
+
 def evaluate(
         env_fn: Callable[[], gym.Env],
+        dt: float,
         epoch: int,
         vfunction: MLP,
         afunction: MLP,
         anoise: Union[ParameterNoise, ActionNoise],
         device: torch.device):
     """ Evaluate. """
-    log = 100
+    log = int(1 / dt)
 
     env = env_fn()
     if epoch % log == log - 1:
-        nb_steps = 500
+        nb_steps = int(4 / dt)
         with torch.no_grad():
             obs = to_tensor(env.reset(), device).unsqueeze(0)
             for _ in range(nb_steps):
@@ -116,7 +132,7 @@ def evaluate(
                 obs, _, _, _ = env.step(action)
                 obs = to_tensor(obs, device).unsqueeze(0)
                 env.render()
-                sleep(.02)
+                sleep(dt)
         env.close()
 
     if isinstance(env, PusherEnv):
@@ -151,6 +167,8 @@ def evaluate(
             plt.imshow(vs, origin='lower')
             plt.colorbar()
             plt.pause(.1)
+            if epoch % log == log - 1:
+                plt.savefig(f'logs/results_{dt}_{epoch}.pdf')
 
 
 def main(
@@ -181,8 +199,8 @@ def main(
     vec_env = SubprocVecEnv(envs)
     obs = vec_env.reset()
 
-    def lr_decay(_):
-        return 1
+    def lr_decay(t):
+        return 1 / np.power(1 + dt * t, 1/2)
 
     def noise_decay(_):
         return 1
@@ -205,27 +223,29 @@ def main(
         a_noise = ActionNoise((batch_size, nb_actions), theta, sigma, dt, noise_decay)
         a_noise_eval = ActionNoise((1, nb_actions), theta, sigma_eval, dt, noise_decay)
 
-    optimizer = torch.optim.SGD(chain(afunction.parameters(), vfunction.parameters()), lr=lr * dt)
-    scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_decay)
+    optimizers = (torch.optim.SGD(afunction.parameters(), lr=lr * dt),
+                  torch.optim.SGD(vfunction.parameters(), lr=lr * dt ** 2))
+    schedulers = (torch.optim.lr_scheduler.LambdaLR(optimizers[0], lr_decay),
+                  torch.optim.lr_scheduler.LambdaLR(optimizers[1], lr_decay))
 
     for e in range(nb_epochs):
         print(f"Epoch {e}...")
-        obs = train(e, vfunction, afunction, a_noise, optimizer, scheduler,
+        obs = train(e, vfunction, afunction, a_noise, optimizers, schedulers,
                     vec_env, obs, dt, gamma,
                     value_floating_mean, nb_steps, device)
-        evaluate(env_fn, e, vfunction, afunction, a_noise_eval, device)
+        evaluate(env_fn, dt, e, vfunction, afunction, a_noise_eval, device)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dt', type=float, default=.05)
     parser.add_argument('--env_id', type=str, default='pendulum')
-    parser.add_argument('--noise_type', type=str, default='action')
+    parser.add_argument('--noise_type', type=str, default='parameter')
     parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--hidden_size', type=int, default=64)
     parser.add_argument('--nb_layers', type=int, default=1)
     parser.add_argument('--gamma', type=float, default=1.)
-    parser.add_argument('--nb_epochs', type=int, default=5000)
+    parser.add_argument('--nb_epochs', type=int, default=50000)
     parser.add_argument('--nb_steps', type=int, default=500)
     parser.add_argument('--sigma_eval', type=float, default=0)
     parser.add_argument('--sigma', type=float, default=.3)
--- pendulum_exp/stats.py
+++ pendulum_exp/stats.py
@@ -1,6 +1,7 @@
 """ Stat utilities. """
 from typing import Optional, Callable
 import numpy as np
+import torch
 
 class FloatingAvg:
     """ Computes floating average of a value. """
@@ -8,7 +9,7 @@ class FloatingAvg:
                  decay: Optional[Callable[[int], float]] = None) -> None:
         self._count = 0
         self._alpha = alpha
-        self._mean = None
+        self._mean = 0
         self._decay = decay
 
     def step(self, value):
@@ -26,9 +27,17 @@ class FloatingAvg:
     @property
     def mean(self) -> np.ndarray:
         """ Returns mean. """
+        if self._mean is None:
+            return 0
         return self._mean
 
 def epsilon_noise(arr: np.ndarray, epsilon: float) -> np.ndarray:
     """ Apply an epsilon greedy noise on arr. """
     mask = (np.random.uniform(0, 1, (arr.shape[0],)) < epsilon)
     return arr * (1 - mask) + (4 * np.random.randint(0, 2, (arr.shape[0],)) - 2) * mask
+
+def penalize_mean(arr: torch.Tensor):
+    """ Introduce a mean penalization. """
+    mask = (torch.zeros_like(arr).uniform_() >= .5)
+    length = min(torch.sum(mask), torch.sum(1 - mask)).item()
+    return (arr[mask][:length] * arr[1 - mask][:length]).mean()
