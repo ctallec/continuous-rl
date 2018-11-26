Fixing normalization

pendulum_exp/models.py
@@ -60,11 +60,31 @@ class ContinuousAdvantageMLP(MLP, ParametricFunction):
     def output_shape(self) -> Shape:
         return ((self._nb_outputs,),)
 
-class NormalizedMLP(ParametricFunction, nn.Module):
+class CustomBN(nn.Module):
+    def __init__(self, nb_feats: int, eps: float=1e-5) -> None:
+        super().__init__()
+        self._count = 0
+        self._eps = eps
+        self.register_buffer('_mean', torch.zeros(nb_feats, requires_grad=False))
+        self.register_buffer('_squared_mean', torch.ones(nb_feats, requires_grad=False))
+
+    def forward(self, *inputs: Tensorable) -> torch.Tensor:
+        device = self._mean.device # type: ignore
+        t_input = check_tensor(inputs[0], device)
+        batch_size = t_input.size(0)
+        std = torch.sqrt(self._squared_mean - self._mean ** 2 + self._eps) # type: ignore
+        output = (t_input - self._mean) / std # type: ignore
+        with torch.no_grad():
+            self._mean = (self._mean * self._count + batch_size * t_input.mean(dim=0)) / (self._count + batch_size) # type: ignore
+            self._squared_mean = (self._squared_mean * self._count + batch_size * (t_input ** 2).mean(dim=0)) / (self._count + batch_size) # type: ignore
+            self._count += batch_size
+        return output
+
+class NormalizedMLP(nn.Module, ParametricFunction):
     def __init__(self, model: ParametricFunction) -> None:
         super().__init__()
         self._model = model
-        self._bns = nn.ModuleList([nn.BatchNorm1d(*feat) for feat in self._model.input_shape()])
+        self._bns = nn.ModuleList([CustomBN(*feat) for feat in self._model.input_shape()])
 
     def forward(self, *inputs: Tensorable):
         device = next(self.parameters())