72836a8992684ea9cca636d0f44ba2485e0bd673
ctallec <corentinxtallec@gmail.com>
2018-11-30 17:57:34 +0100

    Correct hill env

--- pendulum_exp/envs/hill.py
+++ pendulum_exp/envs/hill.py
@@ -8,30 +8,58 @@ import numpy as np
 from abstract import Env
 
 class HillEnv(gym.Env, Env):
-    def __init__(self):
-        self.observation_space = Box(low=-1, high=1, shape=(1,), dtype=np.float32)
-        self.action_space = Discrete(2)
+    def __init__(self, discrete: bool=True) -> None:
+        if discrete:
+            self._action_space = Discrete(2)
+        else:
+            self._action_space = Box(low=-1, high=1, shape=(1,), dtype=np.float32)
+        self._observation_space = Box(low=-1, high=1, shape=(1,), dtype=np.float32)
         self._x = None
         self.dt = .1
         self.seed()
 
-    def step(self, action):
-        assert self.action_space.contains(action)
-        done = False
-        if self._x[0] < 0:
-            done = True
-        elif self._x[0] < 10 * self.dt:
-            self._x = self._x - self.dt
-        else:
-            self._x += 2 * (action - 1) * self.dt
+    @property
+    def action_space(self):
+        return self._action_space
 
-        reward = 1
+    @property
+    def observation_space(self):
+        return self._observation_space
+
+    def action(self, action):
+        if isinstance(self._action_space, Box):
+            return action
+        else:
+            return 2 * action - 1
+
+    def step(self, action):
+        done = False
+
+        # perform action
+        if -1 < self._x[0] < -.25:
+            # if -1 < x < -.25, action has no impact
+            # we go left
+            self._x[0] = self._x[0] - self.dt
+        elif self._x[0] >= 1.:
+            self._x[0] = 1.
+        else:
+            self._x += self.action(action) * self.dt
+
+        # compute reward: if x < 0, r = 1, 0 otherwise
+        if self._x[0] < 0:
+            reward = 1
+        else:
+            reward = 0
+
+        # compute termination: if x <= -1, done
+        if self._x[0] <= -1:
+            done = True
 
         return self._x, reward, done, {}
 
     def reset(self):
         np.random.seed()
-        self._x = np.clip(np.random.normal(0, 1, (1,)), self.dt, 1)
+        self._x = np.clip(np.random.normal(0, 1, (1,)), 0, 1)
         return self._x
 
     def close(self):
