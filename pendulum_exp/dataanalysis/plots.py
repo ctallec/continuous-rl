81ac89d1de9659627ee46d5430b0862da3456f92
Leonard Blier <leonard.blier@gmail.com>
2019-01-10 00:35:41 -0800

    .

--- pendulum_exp/dataanalysis/plots.py
+++ pendulum_exp/dataanalysis/plots.py
@@ -10,11 +10,13 @@ from datetime import datetime
 
 algolabeldict = {
     'discrete_value':"qlearning",
-    'discrete_advantage':"advup"
+    'discrete_advantage':"advup",
+    'approximate_advantage':'advup',
+    'approximate_value':'ddpg',
 }
 
 def plot_learning_curves(expdata, key_list, namefile):
-    mint, maxt = 0, 40
+    mint, maxt = 0, 100
 
     nlines, ncol = len(key_list), 1
     fig, axes = plt.subplots(nlines, ncol, figsize=(5.*ncol, 4.*nlines))
@@ -32,17 +34,24 @@ def plot_learning_curves(expdata, key_list, namefile):
             args = setting.args
             dt = args['dt']
             algo = args["algo"]
-            label = f"{algolabeldict[algo]}; dt={dt:.1e}"
-            if algo == 'discrete_value':
+            label = f"{algolabeldict[algo]}; dt={dt:.0e}"
+            if algo == 'discrete_value' or algo == 'approximate_value':
                 linestyle = '--'
                 c=cm.to_rgba(np.log(dt))
                 linewidth=1.
                 alpha=None
-            elif algo == 'discrete_advantage':
+                if 'tau' in expdata.deltakeys:
+                    tau = setting.args['tau']
+                    label += f';tau{tau}'
+                    if tau != 0:
+                        linestyle = ':'
+            elif algo == 'discrete_advantage' or algo == 'approximate_advantage':
                 linestyle = '-'
                 c=cm.to_rgba(np.log(dt))
                 linewidth=1.
                 alpha = None
+            else:
+                raise ValueError
             timeseq = setting.timeseq(key)
             if timeseq is None:
                 continue
@@ -50,11 +59,12 @@ def plot_learning_curves(expdata, key_list, namefile):
             xdata = np.array([i for (i,v) in timeseq.items()])
             ydata = np.array([v for (i,v) in timeseq.items()])
 
+            kernelsize = max(maxt/100, 0.3) / dt
             x = np.arange(max(xdata)+1)
             yinterp = np.interp(x, xdata, ydata)
-            ysmoothed = gaussian_filter1d(yinterp, sigma=0.3 / dt)
+            ysmoothed = gaussian_filter1d(yinterp, sigma=kernelsize)
 
-            sigma = np.sqrt(gaussian_filter1d((yinterp - ysmoothed) ** 2, sigma=0.3 / dt))
+            sigma = np.sqrt(gaussian_filter1d((yinterp - ysmoothed) ** 2, sigma=kernelsize))
             x = dt * x
             y = dt * ysmoothed
             sigma = dt * sigma
@@ -69,16 +79,50 @@ def plot_learning_curves(expdata, key_list, namefile):
         first = False
 
     plt.tight_layout()
-    plt.savefig(namefile+'.eps', format="eps")
-    plt.savefig(namefile+'.png', format='png')
+    # plt.savefig(namefile+'.eps', format="eps")
+    plt.savefig(namefile+'.png', format='png', dpi=1000)
 
-# start_date = datetime.strptime('2019_01_08_03_13_33', "%Y_%m_%d_%H_%M_%S")
+start_date = datetime.strptime('2019_01_09_05_44_37', "%Y_%m_%d_%H_%M_%S")
 # stop_date = datetime.strptime('2019_01_08_03_13_35', "%Y_%m_%d_%H_%M_%S")
-start_date = 'last'
+# start_date = 'last'
 stop_date = None
 runlist = loader_leonard('/private/home/leonardb/workdir', 'mujoco_continuous', 
     start_date=start_date, stop_date=stop_date)
 
 expdata = ExperimentData(runlist)
 
-plot_learning_curves(expdata, ['Return', 'Return'], 'out')
+print(expdata.deltakeys)
+
+expdata_ant = expdata.filter_settings(lambda s: s['env_id'] == 'ant')
+plot_learning_curves(expdata_ant, ['Return', 'Return'], 'ant')
+
+expdata_cartpole = expdata.filter_settings(lambda s: s['env_id'] == 'cartpole')
+plot_learning_curves(expdata_cartpole, ['Return', 'Return'], 'cartpole')
+
+
+# expdata_tau = expdata.filter_settings(lambda s: s['algo'] == 'discrete_value')
+# expdata_tau.repr_rawlogs("Return", 5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
