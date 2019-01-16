360e569e86a0249c28e456a11c958a8280135949
Leonard Blier <leonard.blier@gmail.com>
2019-01-16 08:26:04 -0800

    script

--- pendulum_exp/dataanalysis/script_leonard.py
+++ pendulum_exp/dataanalysis/script_leonard.py
@@ -7,10 +7,11 @@ from dataloader import loader_leonard, ExperimentData
 from plots import plot_learning_curves
 
 # 
-start_date = datetime.strptime('2019_01_14_10_22_54', "%Y_%m_%d_%H_%M_%S")
-stop_date = datetime.strptime('2019_01_14_10_22_55', "%Y_%m_%d_%H_%M_%S")
-# start_date = 'last'
-# stop_date = None
+# start_date = datetime.strptime('2019_01_14_10_22_54', "%Y_%m_%d_%H_%M_%S")
+# stop_date = datetime.strptime('2019_01_14_10_22_55', "%Y_%m_%d_%H_%M_%S")
+
+start_date = 'last'
+stop_date = None
 runlist = loader_leonard('/private/home/leonardb/workdir', 'mujoco_continuous', 
     start_date=start_date, stop_date=stop_date)
 
@@ -18,14 +19,14 @@ expdata = ExperimentData(runlist)
 
 print(expdata.deltakeys)
 
-expdata_ant = expdata.filter_settings(lambda s: s['env_id'] == 'ant')
-plot_learning_curves(expdata_ant, ['Return', 'Return'], 'ant', gtype='run_std', mint=0, maxt=20)
+# expdata_ant = expdata.filter_settings(lambda s: s['env_id'] == 'ant')
+# plot_learning_curves(expdata_ant, ['Return', 'Return'], 'ant', gtype='run_std', mint=0, maxt=20)
 
 # expdata_cartpole = expdata.filter_settings(lambda s: s['env_id'] == 'cartpole')
 # plot_learning_curves(expdata_cartpole, ['Return', 'Return'], 'cartpole')
 
-# expdata_cheetah = expdata.filter_settings(lambda s: s['env_id'] == 'half_cheetah')
-# plot_learning_curves(expdata_cheetah, ['Return', 'Return'], 'cheetah')
+expdata_cheetah = expdata.filter_settings(lambda s: s['env_id'] == 'half_cheetah')
+plot_learning_curves(expdata_cheetah, ['Return', 'Return'], 'cheetah')
 
 
 # expdata_tau = expdata.filter_settings(lambda s: s['algo'] == 'discrete_value')
