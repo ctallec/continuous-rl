ed211f4a7563d95dd085ac4516f3c1b0f2950e67
ctallec <corentinxtallec@gmail.com>
2019-01-22 15:03:48 +0100

    .

--- pendulum_exp/dataanalysis/script_corentin.py
+++ pendulum_exp/dataanalysis/script_corentin.py
@@ -1,10 +1,11 @@
 import argparse
 from dataloader import loader_leonard, ExperimentData
 from plots import plot_learning_curves
+from typing import List
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--logdir', type=str, required=True)
-parser.add_argument('--exp_name', type=str, required=True)
+parser.add_argument('--exp_names', nargs='+', type=str, required=True)
 parser.add_argument('--std_type', type=str, choices=['time', 'run'])
 parser.add_argument('--min_t', type=float, default=0.)
 parser.add_argument('--max_t', type=float, default=100.)
@@ -12,9 +13,18 @@ args = parser.parse_args()
 
 start_date = 'last'
 stop_date = None
-runlist = loader_leonard(args.logdir, args.exp_name, start_date=start_date, stop_date=stop_date)
+runlist: List[ExperimentData] = []
+for exp_name in args.exp_names:
+    runlist.append(loader_leonard(args.logdir, exp_name, start_date=start_date, stop_date=stop_date))
 
 expdata = ExperimentData(runlist)
+
+def filter(args) -> bool:
+    return (hasattr(args, 'noscale') and args.noscale and 'value' in args.algo) \
+        or 'advantage' in args.algo
+
+
+expdata = expdata.filter_settings(filter)
 
 plot_learning_curves(expdata, ['Return'], args.exp_name, mint=args.min_t, maxt=args.max_t, gtype=args.std_type + "_std")
 expdata.repr_rawlogs("Return", 5)
